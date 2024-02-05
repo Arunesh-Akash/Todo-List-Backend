78c47d699c233ecb14d483316a94e354427e9d4c
Arunesh Akash <akashkr099@gmail.com>
2024-02-06 02:30:00 +0530

    Change the application port

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const signupRouter=require("./Router/SignupRouter");
 const loginRouter=require("./Router/LoginRouter");
 const todoRouter=require("./Router/TodoRouter");
 const userRouter=require("./Router/UserRouter");
+require('dotenv').config();
 
 mongoose.connect("mongodb://localhost:27017/todo-DB");
 
@@ -19,7 +20,7 @@ app.use("/user/todo",todoRouter);
 app.use("/user",userRouter);
 
 
-const APPLICATION_PORT=4500;
+const APPLICATION_PORT=process.env.PORT;
 app.listen(APPLICATION_PORT,()=>{
     console.log("Server is listening on port 4500");
 });
