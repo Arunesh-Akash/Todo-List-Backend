0ca31eae1695d16df427a02715ab42f96a0b17f5
Arunesh Akash <akashkr099@gmail.com>
2024-02-06 14:33:07 +0530

    Solve the error cors

--- app.js
+++ app.js
@@ -11,6 +11,13 @@ require('dotenv').config();
 
 mongoose.connect("mongodb+srv://akashkr099:3iFki1wXzXubJM6v@cluster0.5iqroxk.mongodb.net/?retryWrites=true&w=majority");
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'https://todo-list-ui-5qxg.onrender.com');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
+
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(cors());
